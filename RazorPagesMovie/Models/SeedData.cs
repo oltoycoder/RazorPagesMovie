deaa1608071e41f0cdd4677f4af190c809fbd191
Toyin Ariyo <toyinariyo66@gmail.com>
2018-08-06 15:36:34 +0100

    Add project files.

---
+++ RazorPagesMovie/Models/SeedData.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace RazorPagesMovie.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesMovieContext(serviceProvider.GetRequiredService<DbContextOptions<RazorPagesMovieContext>>()))
+            {
+                //Look for any movies
+                if (context.Movie.Any())
+                {
+                    return; //DB has been seeded
+                }
+
+                context.Movie.AddRange(new Movie
+                {
+                    Title = "Back to the Future",
+                    ReleaseDate = DateTime.Parse("1985-12-4"),
+                    Genre = "Science fiction",
+                    Price = 7.99M
+                },
+
+                new Movie
+                {
+                    Title = "Ghostbusters",
+                    ReleaseDate = DateTime.Parse("1984-3-13"),
+                    Genre = "Comedy",
+                    Price = 8.99M
+                },
+
+                new Movie
+                {
+                    Title = "Back to the Future Part II",
+                    ReleaseDate = DateTime.Parse("1989-11-24"),
+                    Genre = "Science fiction",
+                    Price = 4.99M
+                },
+
+                new Movie
+                {
+                    Title = "Ghostbusters 2",
+                    ReleaseDate = DateTime.Parse("1986-2-23"),
+                    Genre = "Comedy",
+                    Price = 9.99M
+                }
+               );
+                context.SaveChanges();
+            }
+        }
+    }
+}
